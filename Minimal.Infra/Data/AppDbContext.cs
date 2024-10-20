d2bf75c2a6d517e8714e527e71b1e393cda05cbf
shrey <shrey@SHREYALAPPY>
2024-10-20 22:02:47 +0530

    fb

---
+++ Minimal.Infra/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Minimal.Core.Entities;
+
+namespace Minimal.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet for each entity
+        public DbSet<Product> Products { get; set; }  // Example DbSet
+    }
+}
